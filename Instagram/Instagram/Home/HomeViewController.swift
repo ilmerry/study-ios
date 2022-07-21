88622c8129d702467df573de653358a380abcfaa
Eunhyung Choi <milkiss0414@naver.com>
2022-07-21 23:26:54 +0900

    Clone: TableView extension

---
+++ Instagram/Instagram/Home/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by 최은형 on 2022/07/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    
+}
