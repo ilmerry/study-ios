c5e7945e4dcb4f71dafb8fabed2a84e41a396381
Eunhyung Choi <milkiss0414@naver.com>
2022-07-21 23:49:54 +0900

    Clone: FeedTableCell Layout

---
+++ Instagram/Instagram/Home/FeedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.swift
+//  Instagram
+//
+//  Created by 최은형 on 2022/07/21.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
